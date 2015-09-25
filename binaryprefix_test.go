e9ca56f12e384bf635bb5b202b485a90887bdeb1
Raül Pérez <repejota@gmail.com>
2015-09-25 05:08:19 +0200

    Add more tests

--- binaryprefix_test.go
+++ binaryprefix_test.go
@@ -25,3 +25,23 @@ func TestParseBinaryPrefixMB(t *testing.T) {
 		t.Error("1MB are 0.0009765625 GBytes but got ", b.GBytes())
 	}
 }
+
+func TestParseBinaryPrefixGB(t *testing.T) {
+	b, err := ParseBinaryPrefix("4GB")
+	if err != nil {
+		t.Error("Error parsing 4GB")
+	}
+	if int(b) != 4294967296 {
+		t.Error("4GB are 4294967296 Bytes but got ", int(b))
+	}
+}
+
+func TestParseBinaryPrefixTB(t *testing.T) {
+	b, err := ParseBinaryPrefix("3TB")
+	if err != nil {
+		t.Error("Error parsing 3TB")
+	}
+	if int(b) != 3298534883328 {
+		t.Error("3TB are 3298534883328 Bytes but got ", int(b))
+	}
+}
